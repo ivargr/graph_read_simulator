d0103151bbb83be9b3d49e24441dcb2869a7fb14
Ivar Grytten <ivar.grytten@gmail.com>
2019-10-02 20:15:33 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='graph_read_simulator',
       license='MIT',
       packages=['graph_read_simulator'],
       zip_safe=False,
-      install_requires=['numpy', 'pyvg==1.1.2'],
+      install_requires=['numpy', 'pyvg==1.1.2', 'offsetbasedgraph', 'simple_read_mutator', 'pyfaidx'],
       classifiers=[
             'Programming Language :: Python :: 3'
       ],
